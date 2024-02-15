3dc6e7aa7ca765a0c5e419c8204b0478fd941f00
Emilia <emiliazlosada@outlook.com>
2024-02-15 15:16:55 -0600

    Primer archivo guardado en swift, hola mundo

---
+++ introduccionSwift/introduccionSwift/introduccionSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  introduccionSwiftApp.swift
+//  introduccionSwift
+//
+//  Created by ADMIN UNACH on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct introduccionSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
