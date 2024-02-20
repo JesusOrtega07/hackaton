127b9d7200f71c5fce8630a6f03e0932ea097172
Chus <jesus.ortega07@unach.mx>
2024-02-20 17:08:06 -0600

    Documentación de CardView

--- Hiko2/Componet/CardView.swift
+++ Hiko2/Componet/CardView.swift
@@ -10,18 +10,44 @@ import SwiftUI
 struct CardView: View {
     var body: some View {
         
-        ZStack {
+        ZStack { // Zstack principal
             BackgroundView()
+                    
+            VStack { // VStack que separa el texto Haking con la imagen
+                HStack { // HStack que separa Haking y el botón de "Aplicacion [...]"
+                    Text("Haking")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundStyle(
+                            LinearGradient(
+                                colors: [Color.colorGrayMedium,Color.colorGrayLight], 
+                                startPoint: .top,
+                                endPoint: .bottom
+                            )
+                        )
+                    Button{
+                        
+                    }label: {
+                        Text("TextHolder")
+                    }
+                }
                 
-            ZStack {
-                Circle()
-                    .fill(
-                        LinearGradient(
-                            colors: [Color("ColorIndigoMedium"),Color("ColorSalmonLight")], startPoint: .topLeading, endPoint: .bottomTrailing)
+                Text("Aplicacion de prueba desarrollando los conocimientos del curso")
+                    .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                    
+                
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(
+                                colors: [Color("ColorIndigoMedium"),Color("ColorSalmonLight")], startPoint: .topLeading, endPoint: .bottomTrailing)
                     ).frame(width: 250,height: 250)
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                
             }
         }.frame(width: 350, height: 600)
     }
