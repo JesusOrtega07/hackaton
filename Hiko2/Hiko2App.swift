29f0e41aaf9ec4294d9634bc95aae3dd07398922
Chus <jesus.ortega07@unach.mx>
2024-02-20 15:21:14 -0600

    Initial Commit

---
+++ Hiko2/Hiko2App.swift
@@ -0,0 +1,17 @@
+//
+//  Hiko2App.swift
+//  Hiko2
+//
+//  Created by ADMIN UNACH on 20/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Hiko2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
